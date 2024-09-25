e32a3bf95fb1effb5decd098d92eefc06095ce42
autotrends <autotrends.com.sz@gmail.com>
2024-09-25 16:27:16 +0200

    updates

--- reviews/main.js
+++ reviews/main.js
@@ -1,26 +1,21 @@
-// nav background
-let header = document.querySelector("header");
+// Add shadow effect to the header on scroll
+  let header = document.querySelector("header");
+  window.addEventListener("scroll", () => {
+    header.classList.toggle("shadow", window.scrollY > 0);
+  });
 
-window.addEventListener("scroll", () => {
-    header.classList.toggle("shadow", window.scrollY > 0)
-})
-
-//Filter
-$(document).ready(function () {
+  // Filter functionality with jQuery
+  $(document).ready(function () {
     $(".filter-item").click(function () {
-        const value = $(this).attr("data-filter");
-        if (value == "all"){
-            $(".post-box").show("1000")
-        } else{
-            $(".post-box")
-                .not("." + value)
-                .hide(1000);
-            $(".post-box")
-            .filter("." + value)
-            .show("1000")
-        }
+      const value = $(this).attr("data-filter");
+      if (value == "all") {
+        $(".post-box").show("1000");
+      } else {
+        $(".post-box").not("." + value).hide(1000);
+        $(".post-box").filter("." + value).show("1000");
+      }
     });
     $(".filter-item").click(function () {
-        $(this).addClass("active-filter").siblings().removeClass("active-filter")
+      $(this).addClass("active-filter").siblings().removeClass("active-filter");
     });
-});
+  });
